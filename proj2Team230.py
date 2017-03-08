0efe79c4c4bcffdb8781f68dfc329f2779abdaa2
Abraham Medina <abrahamedina24@gmail.com>
2017-03-08 20:19:10 +0000

    Adding final version of html

--- proj2Team230.py
+++ proj2Team230.py
@@ -1,18 +1,56 @@
 import flask
+from flask import *
 import os
 
-proj2Team230 = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
-@proj2Team230.route('/')
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route('/')
 def index():
-    #return "hi guys"
-    return flask.render_template("index.html")
-@proj2Team230.route('/test')
-def test():
-    #return "hi guys"
-    return flask.render_template("test.html")
+    return flask.render_template("upload.html")
+@app.route('/desition')
+def desition():
+    return flask.render_template("desition.html")
+@app.route('/encrypt')
+def encrypt():
+    return flask.render_template("encrypt.html")
+@app.route('/decrypt')
+def decrypt():
+    return flask.render_template("decrypt.html")
+    
+@app.route("/desition", methods = ['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'static/')
+    print(target)
+    
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, 'image.png'])
+        print(destination)
+        file.save(destination)
+    return render_template("desition.html")
+    
+@app.route("/encrypt", methods = ['POST'])
+def runEncrypt():
+    print("Hello")
+    return render_template("encrypt.html")
+@app.route("/decrypt", methods = ['POST'])
+def runDecrypt():
+    print("Hello")
+    return render_template("decrypt.html")
 
-proj2Team230.run(
+if __name__ == "__main__":    
+    app.run(
+    port = int(os.getenv('PORT', 8080)),
+    host = os.getenv('IP', '0.0.0.0')
+    )
+
+app.run(
     port = int(os.getenv('PORT', 8080)),
     host = os.getenv('IP', '0.0.0.0')
 )
